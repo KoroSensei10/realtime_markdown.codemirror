b3d1aa83e1be674c660e9cc5b21e5906dbbcddb4
Mathis <mathisjung02@gmail.com>
2025-10-18 14:06:55 +0200

    demo site setup

---
+++ demo/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Editor from './lib/Editor.svelte';
+</script>
+
+<svelte:head>
+  <title>Real-time Markdown Demo</title>
+  <meta name="description" content="A demo of the real-time Markdown editor using CodeMirror and Svelte." />
+</svelte:head>
+
+<main class="w-screen h-screen bg-neutral-700 text-neutral-200 flex flex-col p-8 gap-4">
+  <h1 class="text-3xl w-fit px-4 py-2 bg-neutral-800 font-bold rounded-2xl shadow-lg">
+    Real-time Markdown Demo
+  </h1>
+  <Editor />
+</main>
---
+++ demo/src/lib/Editor.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import { markdown } from "@codemirror/lang-markdown";
+    import { lineNumbers } from "@codemirror/view";
+    import { EditorView } from "codemirror";
+
+    import { realtimeMarkdown } from "realtime_markdown.codemirror";
+
+    const basicTheme = EditorView.theme({
+        "&": {
+            height: "100%"
+        },
+        ".cm-content": {
+            fontFamily: "ui-monospace, SFMono-Regular, Consolas, 'Liberation Mono', Menlo, Courier, monospace",
+            fontSize: "16px",
+            lineHeight: "1.5",
+            caretColor: "white",
+        },
+        ".cm-gutters": {
+            backgroundColor: "transparent",
+            color: "#6b7280", // Tailwind's neutral-500
+            border: "none",
+        },
+        ".cm-gutterElement": {
+            display: "flex",
+            alignItems: "center",
+        },
+    });
+
+    function mountCodeMirror(node: HTMLElement) {
+        new EditorView({
+            parent: node,
+            doc: '## Welcome to the Real-time Markdown Editor!\n\nStart typing your *Markdown* content here.',
+            extensions: [
+                basicTheme,
+                lineNumbers(),
+                markdown(),
+                realtimeMarkdown,
+            ]
+        })
+    }
+</script>
+
+<div {@attach mountCodeMirror}
+    class="w-full h-full bg-neutral-800 p-2 rounded-2xl shadow-lg overflow-auto"
+>
+
+</div>
